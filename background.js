ad3113ac305845eafb57090cfe64c7db393140e6
DESKTOP-KHGF99R\Gabor Majerszky <majerszkygabor@gmail.com>
2020-05-15 20:57:41 +0100

    Added default project setup

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(function () {
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    let rule = {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({ pageUrl: { hostEquals: "www.youtube.com" } }),
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }
+    chrome.declarativeContent.onPageChanged.addRules([rule]);
+  });
+
+});
